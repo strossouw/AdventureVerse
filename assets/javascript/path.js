1f4c843a79e28685232000dc91933c8b488d87be
strossouw <srossouw@uwm.edu>
2024-05-03 12:08:13 -0500

    added background color and icons

--- assets/javascript/path.js
+++ assets/javascript/path.js
@@ -1,14 +1,15 @@
 // Define a constructor function for creating path objects
-function Path(name, description, length) {
+function Path(name, description, length, icon) {
     this.name = name;
     this.description = description;
     this.length = length;
+    this.icon = icon; // New property for the icon
 }
 
 var paths = [
-    new Path("Train", "Take the train to reach Mother Goose.", "12"),
-    new Path("Walk", "Meander through the woods to reach Mother Goose.", "27"),
-    new Path("Fly", "Fly at night to reach Mother Goose.", "2"),
+    new Path("Train", "Take the train to reach Mother Goose.", "12", "fa-train"),
+    new Path("Walk", "Meander through the woods to reach Mother Goose.", "27", "fa-walking"),
+    new Path("Fly", "Fly at night to reach Mother Goose.", "2", "fa-plane"),
 ];
 
 // Export the path list
