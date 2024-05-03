065285a5a0d06c3e4391b3bcbe98c23f61d20fe1
strossouw <srossouw@uwm.edu>
2024-05-02 22:04:46 -0500

    corrected path function. Added local storage to keep character and number of days throughout story

--- assets/javascript/path.js
+++ assets/javascript/path.js
@@ -10,13 +10,9 @@ var paths = [
     new Path("Train", "Take the train to reach Mother Goose.", "long"),
     new Path("Walk", "Meander through the woods to reach Mother Goose.", "medium"),
     new Path("Fly", "Fly at night to reach Mother Goose.", "short"),
-    new Path("Boat", "Sail across the river to reach Mother Goose.", "medium"),
-    new Path("Bike", "Cycle through the countryside to reach Mother Goose.", "medium"),
-    new Path("Teleport", "Use teleportation magic to instantly reach Mother Goose.", "short")
 ];
 
-// Function to randomly select three paths
-function getRandomPaths() {
-    var shuffledPaths = paths.sort(() => 0.5 - Math.random()); // Shuffle the paths array
-    return shuffledPaths.slice(0, 3); // Select the first three paths
+// Export the path list
+function getPathList() {
+    return paths;
 }
